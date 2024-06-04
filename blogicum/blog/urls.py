bf88a8dd2ff1035e2d31eb631c7dbee5e08f22e3
Aleks-Orlov <lexx4432@ya.ru>
2024-06-05 01:58:34 +0300

    add apps blog, pages

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('blog/postst/<int:id>/', views.post_detail, name='post_detail'),
+    path('blog/category/<slug:category_slug>/', views.category_post, name='category_post')
+]
